0ba7bb955f1f933fec45e4174eca60dcb9653add
robdan89 <fuquardwork@gmail.com>
2020-05-08 10:14:52 -0500

    user can now remove ingredient from ingredients.

--- src/components/pantry/Pantry.js
+++ src/components/pantry/Pantry.js
@@ -5,36 +5,36 @@ import "../PantryPal.css";
 import MealMakerForm from "./mealMaker/MealMakerForm";
 import { MealQuantityProvider } from "./MealQuantityProvider";
 
-
 export default () => {
-    const [ingredients, setIngredients] = useState([]);
-    const addIngredient = (food) => {
-        // copy current state with slice
-        const newIngredients = ingredients.slice();
-        if (!newIngredients.includes(food)) {
-            // if newIngredients doesn not include food...
-            // add new ingredient to copy
-            newIngredients.push(food);
-            // invoke setIngredients and pass the copy as the argument
-            setIngredients(newIngredients);
-        }
-    };
-    const removeIngredient = (food) => {
-        const copy = ingredients.slice();
-        const i = copy.splice(i, 1);
-        setIngredients(copy);
-    };
+  const [ingredients, setIngredients] = useState([]);
+  const addIngredient = (food) => {
+    // copy current state with slice
+    const newIngredients = ingredients.slice();
+    if (!newIngredients.includes(food)) {
+      // if newIngredients doesn not include food...
+      // add new ingredient to copy
+      newIngredients.push(food);
+      // invoke setIngredients and pass the copy as the argument
+      setIngredients(newIngredients);
+    }
+  };
+  const removeIngredient = (food) => {
+    const copy = ingredients.slice();
+    const i = copy.indexOf(food);
+    copy.splice(i, 1);
+    setIngredients(copy);
+  };
 
-    return (
-        <>
-            <MealQuantityProvider>
-                <FoodList addIngredient={addIngredient} />
-                <MealMakerForm
-                    addIngredient={addIngredient}
-                    ingredients={ingredients}
-                    removeIngredient={removeIngredient}
-                />
-            </MealQuantityProvider>
-        </>
-    );
+  return (
+    <>
+      <MealQuantityProvider>
+        <FoodList addIngredient={addIngredient} />
+        <MealMakerForm
+          addIngredient={addIngredient}
+          ingredients={ingredients}
+          removeIngredient={removeIngredient}
+        />
+      </MealQuantityProvider>
+    </>
+  );
 };
