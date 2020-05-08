2d4e3ff4858e35a3dcc4bbf3700825bed1d8e34d
Steve Brownlee <chortlehoort@gmail.com>
2020-05-07 23:08:48 -0500

    Switched from custom hook to new context component

--- src/components/pantry/useMealMaker.js
+++ src/components/pantry/useMealMaker.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 
 export const useMealMaker = () => {
-  const [mealTrackerObject, setMealTrackerObject] = useState({});
-  const mealMakerTracker = (foodId, quantity) => {
-    // copy the existing state with object.assign
-    // on new obj add a new id which is foodId and its value is quantity
-    const copy = Object.assign(mealTrackerObject, { [foodId]: quantity });
-    // then envoke setMealTrackerObject passing this new object as the argument
-    setMealTrackerObject(copy);
-  };
-  return { mealTrackerObject, mealMakerTracker };
+    const [mealTrackerObject, setMealTrackerObject] = useState({});
+
+    const mealMakerTracker = (foodId, quantity) => {
+        const copy = Object.assign({}, { ...mealTrackerObject }, { [foodId]: quantity });
+        setMealTrackerObject(copy);
+    };
+
+    return { mealTrackerObject, mealMakerTracker };
 };
